094b053ddd1c3379e6a5e6a6cf12755abeb039cf
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2025-01-07 17:50:28 +0000

    Published from Shuffle Editor

---
+++ src/components/stats/IndexSectionStats14.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function IndexSectionStats14() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="py-12 lg:py-24 bg-orange-50">
+  <div className="container mx-auto px-4">
+    <div className="flex flex-wrap -mx-4">
+      <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-10 md:mb-0">
+        <div className="text-center">
+          <h5 className="text-2xl xs:text-3xl lg:text-4xl xl:text-5xl mb-4">8</h5>
+          <span className="text-base lg:text-lg text-gray-700">Jaar ervaring</span>
+        </div>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-10 md:mb-0">
+        <div className="text-center">
+          <h5 className="text-2xl xs:text-3xl lg:text-4xl xl:text-5xl mb-4">150+</h5>
+          <span className="text-base lg:text-lg text-gray-700">Projecten afgerond</span>
+        </div>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-10 sm:mb-0">
+        <div className="text-center">
+          <h5 className="text-2xl xs:text-3xl lg:text-4xl xl:text-5xl mb-4">Snelle Support</h5>
+          <span className="text-base lg:text-lg text-gray-700">Renewable Energy Generated</span>
+        </div>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 px-4">
+        <div className="text-center">
+          <h5 className="text-2xl xs:text-3xl lg:text-4xl xl:text-5xl mb-4">100% ROI</h5>
+          <span className="text-base lg:text-lg text-gray-700">Renewable Energy Generated</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
